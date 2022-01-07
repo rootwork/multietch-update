46986f445f5818894f37f0147c37eaf3a2a651e2
Ivan Boothe <git@rootwork.org>
2022-01-06 18:44:05 -0800

    updated rewrites
    Signed-off-by: Ivan Boothe <git@rootwork.org>

--- rewrite.sh
+++ rewrite.sh
@@ -2,13 +2,14 @@
 
 set -e
 cd docs
-find . -regex ".*\.\(html\|css\|js\)$" -type f -print0 | xargs -0 sed -i 's;https://images.squarespace-cdn.com/content;/files/content;g'
-find . -regex ".*\.\(html\|css\|js\)$" -type f -print0 | xargs -0 sed -i 's;https://assets.squarespace.com;/files/assets;g'
+find . -regex ".*\.\(html\|css\|js\)$" -type f -print0 | xargs -0 sed -i 's;https://images.squarespace-cdn.com;/files;g'
 find . -regex ".*\.\(html\|css\|js\)$" -type f -print0 | xargs -0 sed -i 's;http://static1.squarespace.com;/files;g'
 find . -regex ".*\.\(html\|css\|js\)$" -type f -print0 | xargs -0 sed -i 's;https://static1.squarespace.com;/files;g'
+find . -regex ".*\.\(html\|css\|js\)$" -type f -print0 | xargs -0 sed -i 's;https://assets.squarespace.com;/files/assets;g'
 find . -name "*.html" -type f -print0 | xargs -0 sed -i 's;href="https://www.multietch.com/;href="/;g'
 find . -name "*.html" -type f -print0 | xargs -0 sed -i 's;href="https://www.multietch.com;href="/;g'
 find . -name "*.html" -type f -print0 | xargs -0 sed -i 's;href="../s/;href="/files/s/;g'
+find . -name "*.html" -type f -print0 | xargs -0 sed -i 's;href="../s;href="/files/s;g'
 find . -name "*.html" -type f -print0 | xargs -0 sed -i 's;href="../static1.squarespace.com;href="/files;g'
 find . -name "*.html" -type f -print0 | xargs -0 sed -i 's;src="../static1.squarespace.com;src="/files;g'
 find . -name "*.html" -type f -print0 | xargs -0 sed -i 's;src="../assets.squarespace.com;src="/files/assets;g'
