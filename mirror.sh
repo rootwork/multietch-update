a7d4090a02e275304384286ec7bdf5cb8d440924
Ivan Boothe <git@rootwork.org>
2022-01-06 17:23:49 -0800

    adding some additional options to the script
    Signed-off-by: Ivan Boothe <git@rootwork.org>

--- mirror.sh
+++ mirror.sh
@@ -8,4 +8,4 @@
 
 set -e
 
-wget --recursive -c --level=inf --page-requisites --convert-links --adjust-extension --span-hosts --wait=3 --random-wait --continue --domains=multietch.com,squarespace-cdn.com,static1.squarespace.com,assets.squarespace.com https://www.multietch.com
+wget --recursive --level=inf --no-parent --page-requisites --convert-links --adjust-extension --continue --no-clobber --wait=3 --random-wait --span-hosts --domains=multietch.com,squarespace-cdn.com,static1.squarespace.com,assets.squarespace.com https://www.multietch.com
