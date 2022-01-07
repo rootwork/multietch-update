b06c3e6ad2e5bf71eae79ceea6881c0d7832d084
Ivan Boothe <git@rootwork.org>
2022-01-07 15:33:41 -0800

    no longer need copy removal
    Signed-off-by: Ivan Boothe <git@rootwork.org>

--- copy.sh
+++ copy.sh
@@ -6,4 +6,3 @@ cp -r static1.squarespace.com/* docs/files
 cp -r assets.squarespace.com/* docs/files/assets
 cp -r www.multietch.com/* docs
 mv docs/s docs/files
-find . -regex ".*\.\(htmlE\|cssE\|jsE\)$" -type f -delete
