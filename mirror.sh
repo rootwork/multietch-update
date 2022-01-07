b0df694ff0c6e5b365f0995d0370617c6d4b9b87
Ivan Boothe <git@rootwork.org>
2022-01-06 16:06:01 -0800

    adding random wait
    Signed-off-by: Ivan Boothe <git@rootwork.org>

--- mirror.sh
+++ mirror.sh
@@ -1,12 +1,8 @@
 #!/usr/bin/env bash
 # https://gist.github.com/aerickson/37d911e61731a32a04d2ed6cbe9d5378
+# https://stackoverflow.com/a/31746893
+# https://stackoverflow.com/a/70381069
 
 set -e
 
-wget --recursive -c --level=inf --page-requisites --convert-links --adjust-extension --span-hosts --domains=multietch.com,squarespace-cdn.com,static1.squarespace.com,assets.squarespace.com https://www.multietch.com
-
-# If you see 429 errors ("Too many requests") try including:
-#   --wait=[seconds] --random-wait --continue ${https://www.multietch.com}
-# to address the issue.
-# https://stackoverflow.com/a/31746893
-# https://stackoverflow.com/a/70381069
+wget --recursive -c --level=inf --page-requisites --convert-links --adjust-extension --span-hosts --wait=1 --random-wait --continue --domains=multietch.com,squarespace-cdn.com,static1.squarespace.com,assets.squarespace.com https://www.multietch.com
